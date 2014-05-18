a102115083929bf559cd4cf13f71532c4a0fefbb
Carlo De Gregorio <cdegregorio@gmail.com>
2014-05-17 18:03:58 -0700

    --- git asked me to commit

--- Calc/calculations.js
+++ Calc/calculations.js
@@ -5,3 +5,16 @@ module.exports.add = function(x, y) {
 module.exports.subtract = function(x, y) {
 	return x - y;
 };
+
+module.exports.multiply = function(x, y) {
+	return x * y;
+};
+
+module.exports.divide = function(x, y) {
+	return x / y;
+};
+
+
+module.exports.exponent = function(x, y) {
+	return x ^ y;
+};
